235c93426a5f7b3ce744aae373bda7d9aab26afe
larebear007 <larencozart@gmail.com>
2024-04-15 14:15:59 -0500

    Add public dir with images and stylesheets, add views dir with pug templates

--- hello.js
+++ hello.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const morgan = require('morgan');
 const app = express();
 
-// setting pug as templating enging
-app.set("view", "./views");
+// setting pug as templating engine
+app.set("views", "./views");
 app.set("view engine", "pug");
 
-// route
+// static assests lookup & morgan logging
+app.use(express.static("public"));
+app.use(morgan("common"));
+
+// routes
 app.get('/', (req, res) => {
+  res.redirect("/english");
+});
+
+app.get('/english', (req, res) => {
   res.render("hello-world-english");
+});
+
+app.get('/french', (req, res) => {
+  res.render("hello-world-french");
+});
+
+app.get('/serbian', (req, res) => {
+  res.render("hello-world-serbian");
+});
+
+app.get('/spanish', (req, res) => {
+  res.render("hello-world-spanish");
 });
 
 // server
