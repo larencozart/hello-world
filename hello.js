f9a16043ac9d23802565a8251613ac4cd5b563d4
larebear007 <larencozart@gmail.com>
2024-04-17 19:22:32 -0500

    Add loop in nav element of layout.pug

--- hello.js
+++ hello.js
@@ -2,6 +2,34 @@ const express = require("express");
 const morgan = require('morgan');
 const app = express();
 
+
+const COUNTRY_DATA = [
+  { 
+    path: "/english",
+    flag: "flag-of-United-States-of-America.png",
+    alt: "US Flag",
+    title: "Go to US English site"
+  },
+  { 
+    path: "/french",
+    flag: "flag-of-France.png",
+    alt: "Drapeau de la france",
+    title: "Aller sur le site français"
+  },
+  { 
+    path: "/serbian",
+    flag: "flag-of-Serbia.png",
+    alt: "Застава Србије",
+    title: "Идите на српски сајт", 
+  },
+  { 
+    path: "/spanish",
+    flag: "flag-of-Spain.png",
+    alt: "La bandera de España",
+    title: "Redirigir al sito en español"
+  }
+]
+
 // setting pug as templating engine
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -23,6 +51,7 @@ app.get('/', (req, res) => {
 
 app.get('/english', (req, res) => {
   res.render("hello-world-english", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: 'en-US'
   });
@@ -30,6 +59,7 @@ app.get('/english', (req, res) => {
 
 app.get('/french', (req, res) => {
   res.render("hello-world-french", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: 'fr-FR'
   });
@@ -37,6 +67,7 @@ app.get('/french', (req, res) => {
 
 app.get('/serbian', (req, res) => {
   res.render("hello-world-serbian", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: "sr-Cyrl-rs"
   });
@@ -44,6 +75,7 @@ app.get('/serbian', (req, res) => {
 
 app.get('/spanish', (req, res) => {
   res.render("hello-world-spanish", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: "es-ES"
   });
